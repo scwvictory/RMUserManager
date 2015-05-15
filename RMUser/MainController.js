139c9783506dedf14122361a57252b9f3bbc1edf
Tetsuya Matsushita <t.matsushita@newdaysys.co.jp>
2015-05-15 17:38:34 +0900

    RMUserプロジェクトを、ASP.NET MVC→TypeScript(+ AngularJS)に変更(内容はToDoアプリのサンプルになっているが、、、)

---
+++ RMUser/MainController.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=MainController.js.map
---
+++ RMUser/app.ts
@@ -0,0 +1,32 @@
+﻿angular.module("App", []).controller("MainController",
+    ["$scope", "$filter", function ($scope: any, $filter: any) {
+        $scope.todos = [];
+        $scope.addTodo = function () {
+            $scope.todos.push({
+                title: $scope.newTitle,
+                done: false
+            });
+            $scope.newTitle = "";
+        };
+
+        $scope.filter = {
+            done: { done: true },
+            remaining: { done: false }
+        };
+
+        $scope.currentFilter = null;
+
+        $scope.changeFilter = function (filter) {
+            $scope.currentFilter = filter;
+        }
+
+        var where = $filter("filter");
+        $scope.$watch("todos", function (todos) {
+            var length = todos.length;
+
+            $scope.allCount = length;
+            $scope.doneCount = where(todos, $scope.filter.done).length;
+            $scope.remainingCount = length - $scope.doneCount;
+        }, true);
+    }
+]);
